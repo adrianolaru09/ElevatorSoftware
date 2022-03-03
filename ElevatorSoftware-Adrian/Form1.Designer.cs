e44ce149e4fb6012fc7c1e7296bff0f55cfc1986
adrianolaru09 <79530681+adrianolaru09@users.noreply.github.com>
2022-03-03 18:18:51 +0000

    Add files via upload
    Full elevator software

---
+++ ElevatorSoftware-Adrian/Form1.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace ElevatorSoftware_Adrian
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btn_Down = new System.Windows.Forms.Button();
+            this.lbl_Floor1 = new System.Windows.Forms.Label();
+            this.ControlPanel = new System.Windows.Forms.Panel();
+            this.btn_Floor0_panel = new System.Windows.Forms.Button();
+            this.btn_Floor1_panel = new System.Windows.Forms.Button();
+            this.lbl_Control = new System.Windows.Forms.Label();
+            this.lbl_ControlPanel = new System.Windows.Forms.Label();
+            this.Floor1 = new System.Windows.Forms.Panel();
+            this.p_RightDoor = new System.Windows.Forms.PictureBox();
+            this.p_LeftDoor = new System.Windows.Forms.PictureBox();
+            this.btn_Up = new System.Windows.Forms.Button();
+            this.lbl_Floor0 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lst_Events = new System.Windows.Forms.ListBox();
+            this.lbl_Events = new System.Windows.Forms.Label();
+            this.btn_LoadEvents = new System.Windows.Forms.Button();
+            this.btn_ClearLog = new System.Windows.Forms.Button();
+            this.tmr_Anim = new System.Windows.Forms.Timer(this.components);
+            this.ControlPanel.SuspendLayout();
+            this.Floor1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.p_RightDoor)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p_LeftDoor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_Down
+            // 
+            this.btn_Down.Location = new System.Drawing.Point(305, 144);
+            this.btn_Down.Name = "btn_Down";
+            this.btn_Down.Size = new System.Drawing.Size(75, 23);
+            this.btn_Down.TabIndex = 2;
+            this.btn_Down.Text = "Call";
+            this.btn_Down.UseVisualStyleBackColor = true;
+            this.btn_Down.Click += new System.EventHandler(this.btn_Down_Click);
+            // 
+            // lbl_Floor1
+            // 
+            this.lbl_Floor1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbl_Floor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Floor1.Location = new System.Drawing.Point(309, 30);
+            this.lbl_Floor1.Name = "lbl_Floor1";
+            this.lbl_Floor1.Size = new System.Drawing.Size(60, 36);
+            this.lbl_Floor1.TabIndex = 4;
+            this.lbl_Floor1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_Floor1.Click += new System.EventHandler(this.lbl_Floor1_Click);
+            // 
+            // ControlPanel
+            // 
+            this.ControlPanel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ControlPanel.Controls.Add(this.btn_Floor0_panel);
+            this.ControlPanel.Controls.Add(this.btn_Floor1_panel);
+            this.ControlPanel.Controls.Add(this.lbl_Control);
+            this.ControlPanel.Controls.Add(this.lbl_ControlPanel);
+            this.ControlPanel.Location = new System.Drawing.Point(36, 153);
+            this.ControlPanel.Name = "ControlPanel";
+            this.ControlPanel.Size = new System.Drawing.Size(155, 213);
+            this.ControlPanel.TabIndex = 6;
+            // 
+            // btn_Floor0_panel
+            // 
+            this.btn_Floor0_panel.Location = new System.Drawing.Point(41, 154);
+            this.btn_Floor0_panel.Name = "btn_Floor0_panel";
+            this.btn_Floor0_panel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Floor0_panel.TabIndex = 3;
+            this.btn_Floor0_panel.Text = "0";
+            this.btn_Floor0_panel.UseVisualStyleBackColor = true;
+            this.btn_Floor0_panel.Click += new System.EventHandler(this.btn_Floor0_panel_Click);
+            // 
+            // btn_Floor1_panel
+            // 
+            this.btn_Floor1_panel.Location = new System.Drawing.Point(41, 117);
+            this.btn_Floor1_panel.Name = "btn_Floor1_panel";
+            this.btn_Floor1_panel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Floor1_panel.TabIndex = 2;
+            this.btn_Floor1_panel.Text = "1";
+            this.btn_Floor1_panel.UseVisualStyleBackColor = true;
+            this.btn_Floor1_panel.Click += new System.EventHandler(this.btn_Floor1_panel_Click);
+            // 
+            // lbl_Control
+            // 
+            this.lbl_Control.AutoSize = true;
+            this.lbl_Control.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lbl_Control.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Control.Location = new System.Drawing.Point(62, 57);
+            this.lbl_Control.Name = "lbl_Control";
+            this.lbl_Control.Size = new System.Drawing.Size(0, 24);
+            this.lbl_Control.TabIndex = 1;
+            this.lbl_Control.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_Control.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_ControlPanel
+            // 
+            this.lbl_ControlPanel.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lbl_ControlPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ControlPanel.Location = new System.Drawing.Point(24, 12);
+            this.lbl_ControlPanel.Name = "lbl_ControlPanel";
+            this.lbl_ControlPanel.Size = new System.Drawing.Size(110, 26);
+            this.lbl_ControlPanel.TabIndex = 0;
+            this.lbl_ControlPanel.Text = "ControlPanel";
+            this.lbl_ControlPanel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_ControlPanel.Click += new System.EventHandler(this.lbl_ControlPanel_Click);
+            // 
+            // Floor1
+            // 
+            this.Floor1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.Floor1.Controls.Add(this.p_RightDoor);
+            this.Floor1.Controls.Add(this.p_LeftDoor);
+            this.Floor1.Controls.Add(this.btn_Up);
+            this.Floor1.Controls.Add(this.lbl_Floor0);
+            this.Floor1.Controls.Add(this.btn_Down);
+            this.Floor1.Controls.Add(this.lbl_Floor1);
+            this.Floor1.Controls.Add(this.label2);
+            this.Floor1.Location = new System.Drawing.Point(242, 45);
+            this.Floor1.Name = "Floor1";
+            this.Floor1.Size = new System.Drawing.Size(395, 469);
+            this.Floor1.TabIndex = 7;
+            this.Floor1.Paint += new System.Windows.Forms.PaintEventHandler(this.Floor1_Paint);
+            // 
+            // p_RightDoor
+            // 
+            this.p_RightDoor.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.p_RightDoor.Location = new System.Drawing.Point(146, 304);
+            this.p_RightDoor.Name = "p_RightDoor";
+            this.p_RightDoor.Padding = new System.Windows.Forms.Padding(150);
+            this.p_RightDoor.Size = new System.Drawing.Size(74, 122);
+            this.p_RightDoor.TabIndex = 7;
+            this.p_RightDoor.TabStop = false;
+            this.p_RightDoor.Click += new System.EventHandler(this.p_RightDoor_Click);
+            // 
+            // p_LeftDoor
+            // 
+            this.p_LeftDoor.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.p_LeftDoor.Location = new System.Drawing.Point(77, 304);
+            this.p_LeftDoor.Name = "p_LeftDoor";
+            this.p_LeftDoor.Padding = new System.Windows.Forms.Padding(150);
+            this.p_LeftDoor.Size = new System.Drawing.Size(74, 122);
+            this.p_LeftDoor.TabIndex = 6;
+            this.p_LeftDoor.TabStop = false;
+            // 
+            // btn_Up
+            // 
+            this.btn_Up.Location = new System.Drawing.Point(305, 403);
+            this.btn_Up.Name = "btn_Up";
+            this.btn_Up.Size = new System.Drawing.Size(75, 23);
+            this.btn_Up.TabIndex = 3;
+            this.btn_Up.Text = "Call";
+            this.btn_Up.UseVisualStyleBackColor = true;
+            this.btn_Up.Click += new System.EventHandler(this.btn_Up_Click);
+            // 
+            // lbl_Floor0
+            // 
+            this.lbl_Floor0.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbl_Floor0.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Floor0.Location = new System.Drawing.Point(309, 252);
+            this.lbl_Floor0.Name = "lbl_Floor0";
+            this.lbl_Floor0.Size = new System.Drawing.Size(60, 36);
+            this.lbl_Floor0.TabIndex = 5;
+            this.lbl_Floor0.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lbl_Floor0.Click += new System.EventHandler(this.lbl_Floor0_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(389, 23);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lst_Events
+            // 
+            this.lst_Events.BackColor = System.Drawing.SystemColors.Window;
+            this.lst_Events.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lst_Events.FormattingEnabled = true;
+            this.lst_Events.HorizontalScrollbar = true;
+            this.lst_Events.ItemHeight = 16;
+            this.lst_Events.Location = new System.Drawing.Point(697, 153);
+            this.lst_Events.Name = "lst_Events";
+            this.lst_Events.Size = new System.Drawing.Size(389, 212);
+            this.lst_Events.TabIndex = 9;
+            this.lst_Events.SelectedIndexChanged += new System.EventHandler(this.lst_Events_SelectedIndexChanged);
+            // 
+            // lbl_Events
+            // 
+            this.lbl_Events.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.lbl_Events.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Events.Location = new System.Drawing.Point(738, 114);
+            this.lbl_Events.Name = "lbl_Events";
+            this.lbl_Events.Size = new System.Drawing.Size(110, 26);
+            this.lbl_Events.TabIndex = 4;
+            this.lbl_Events.Text = "Events history";
+            this.lbl_Events.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_LoadEvents
+            // 
+            this.btn_LoadEvents.Location = new System.Drawing.Point(697, 389);
+            this.btn_LoadEvents.Name = "btn_LoadEvents";
+            this.btn_LoadEvents.Size = new System.Drawing.Size(75, 23);
+            this.btn_LoadEvents.TabIndex = 10;
+            this.btn_LoadEvents.Text = "Load Events";
+            this.btn_LoadEvents.UseVisualStyleBackColor = true;
+            this.btn_LoadEvents.Click += new System.EventHandler(this.btn_LoadEvents_Click);
+            // 
+            // btn_ClearLog
+            // 
+            this.btn_ClearLog.Location = new System.Drawing.Point(808, 389);
+            this.btn_ClearLog.Name = "btn_ClearLog";
+            this.btn_ClearLog.Size = new System.Drawing.Size(75, 23);
+            this.btn_ClearLog.TabIndex = 11;
+            this.btn_ClearLog.Text = "Clear List";
+            this.btn_ClearLog.UseVisualStyleBackColor = true;
+            this.btn_ClearLog.Click += new System.EventHandler(this.btn_ClearLog_Click);
+            // 
+            // tmr_Anim
+            // 
+            this.tmr_Anim.Enabled = true;
+            this.tmr_Anim.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 563);
+            this.Controls.Add(this.btn_ClearLog);
+            this.Controls.Add(this.btn_LoadEvents);
+            this.Controls.Add(this.lbl_Events);
+            this.Controls.Add(this.lst_Events);
+            this.Controls.Add(this.Floor1);
+            this.Controls.Add(this.ControlPanel);
+            this.Name = "Form1";
+            this.Text = "AdrianOlaru-ElevatorSoftware";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ControlPanel.ResumeLayout(false);
+            this.ControlPanel.PerformLayout();
+            this.Floor1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.p_RightDoor)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p_LeftDoor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_Down;
+        private System.Windows.Forms.Label lbl_Floor1;
+        private System.Windows.Forms.Panel ControlPanel;
+        private System.Windows.Forms.Panel Floor1;
+        private System.Windows.Forms.Label lbl_ControlPanel;
+        private System.Windows.Forms.Button btn_Floor0_panel;
+        private System.Windows.Forms.Button btn_Floor1_panel;
+        private System.Windows.Forms.Label lbl_Control;
+        private System.Windows.Forms.ListBox lst_Events;
+        private System.Windows.Forms.Label lbl_Events;
+        private System.Windows.Forms.Button btn_LoadEvents;
+        private System.Windows.Forms.Button btn_ClearLog;
+        private System.Windows.Forms.Timer tmr_Anim;
+        private System.Windows.Forms.Label lbl_Floor0;
+        private System.Windows.Forms.Button btn_Up;
+        private System.Windows.Forms.PictureBox p_LeftDoor;
+        private System.Windows.Forms.PictureBox p_RightDoor;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ElevatorSoftware-Adrian/Form1.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElevatorSoftware_Adrian
+{
+    public partial class Form1 : Form
+    {
+        private int floorLevel = 0;
+        
+        private DataSet ds = new DataSet();
+        private string dbconnection ;
+        private string dbcommand;
+        private OleDbConnection conn;
+        private OleDbCommand comm;
+        private OleDbDataAdapter adapter;
+        private OleDbCommandBuilder builder;
+
+        private DateTime localDate;
+        private int states = 0;
+        private int a = 0;
+        private int down = 300;
+        private int top = 50;
+        private bool btn0Clicked;
+        private bool btn1Clicked;
+        private bool btnUpClicked;
+        private bool btnDownClicked;
+        public Form1()
+        {
+            InitializeComponent();
+            //timer for elevator animation
+            Timer tmr = new Timer();
+            tmr.Interval = 200;
+            tmr.Tick += new EventHandler(tmr_Tick);
+            tmr.Start();
+            //display the current floor
+            lbl_Floor0.Text = floorLevel.ToString();
+            lbl_Control.Text = floorLevel.ToString();
+            lbl_Floor1.Text = floorLevel.ToString();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //database connection with dataset
+            dbconnection = "Provider=Microsoft.ACE.OLEDB.12.0;" + @"data source= ElevatorLog.accdb ";
+            dbcommand = " Select Part, ActionMove, DateOfAct from ElevatorLog;";
+            try
+            {
+                conn = new OleDbConnection(dbconnection);
+                comm = new OleDbCommand(dbcommand, conn);
+                adapter = new OleDbDataAdapter(comm);
+                load_db_to_ds();
+                builder = new OleDbCommandBuilder(adapter);
+            }
+            catch ( Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        void load_db_to_ds() //load data into dataset
+        {
+            try
+            {
+                adapter.Fill(ds); //fill dataset
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        public void reload_db_to_ds()   //force reload data into  dataset
+        {
+            ds.Clear();
+            load_db_to_ds();
+        }
+        public void save_data_db()
+        {
+            try
+            {
+                //update db
+                DataSet dataSetChanges = ds.GetChanges();
+                adapter.Update(dataSetChanges);
+                ds.AcceptChanges(); //accept changes
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void DisableButtons() //disable all buttons for elevator
+        {
+            btn_Down.Enabled = false;
+            btn_Up.Enabled = false;
+            btn_Floor0_panel.Enabled = false;
+            btn_Floor1_panel.Enabled = false;
+        }
+        private void EnableButtons() // enable all buttons for elevator
+        {
+            btn_Down.Enabled = true;
+            btn_Up.Enabled = true;
+            btn_Floor0_panel.Enabled = true;
+            btn_Floor1_panel.Enabled = true;
+        }
+        private void tmr_Tick(object sender , EventArgs e)
+        {
+            if(p_RightDoor.Top>top && (btn1Clicked == true || btnDownClicked==true) )  
+            {          // move elevator to first floor if the call button is pressed        
+                       // or if the button on the control panel it s pressed
+                p_RightDoor.Top -= 10;
+                p_LeftDoor.Top -= 10;
+                DisableButtons();
+            } else if (btn1Clicked == true || btnDownClicked == true)
+            {
+                btn1Clicked = false;
+                btnDownClicked = false;
+                System.Threading.Thread.Sleep(500);  // wait command
+                OpenCloseDoors();
+                EnableButtons();
+            }
+            if (p_RightDoor.Top < down && (btn0Clicked == true || btnUpClicked == true)) 
+            {   //move elevator to ground floor if the call button is pressed
+                //or if the button on the control panel is pressed
+                p_RightDoor.Top += 10;
+                p_LeftDoor.Top += 10;
+                DisableButtons();
+            }
+            else if (btn0Clicked == true || btnUpClicked == true)
+            {
+                btn0Clicked = false;
+                btnUpClicked = false;
+                System.Threading.Thread.Sleep(500); // wait command
+                OpenCloseDoors();
+                EnableButtons();
+            }
+        }
+        private void lbl_Floor0_Click(object sender, EventArgs e)
+        {
+        }
+        private void lbl_ControlPanel_Click(object sender, EventArgs e)
+        {
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+        private void OpenCloseDoors() // open and close the doors
+        {
+                p_LeftDoor.Left -= 50;
+                p_RightDoor.Left += 50;
+                System.Threading.Thread.Sleep(1000); // wait command
+                p_LeftDoor.Left += 50;
+                p_RightDoor.Left -= 50;
+
+        }
+        private void btn_Up_Click(object sender, EventArgs e)
+        {
+            btn0Clicked = false;
+            btn1Clicked = false;
+            btnUpClicked = true;
+            btnDownClicked = false;
+            if (floorLevel == 1)
+            {   //set the new floor level and display
+                floorLevel = 0;
+                lbl_Floor0.Text = floorLevel.ToString();
+                lbl_Control.Text = floorLevel.ToString();
+                lbl_Floor1.Text = floorLevel.ToString();
+                System.Threading.Thread.Sleep(500);
+            }
+            states = 1; //set the action state for the elevator
+            Insert_In_Log(); //insert into the database
+        }
+        private void btn_Down_Click(object sender, EventArgs e)
+        {
+            btn0Clicked = false;
+            btn1Clicked = false;
+            btnUpClicked = false;
+            btnDownClicked = true;
+            if (floorLevel == 0)
+            {
+                //set the new floor level and display
+                floorLevel = 1;
+                lbl_Floor0.Text = floorLevel.ToString();
+                lbl_Control.Text = floorLevel.ToString();
+                lbl_Floor1.Text = floorLevel.ToString();
+                System.Threading.Thread.Sleep(500);
+            }
+            states = 2; //set the action state for the elevator
+            Insert_In_Log(); //insert into the database
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+        }
+        private void p_RightDoor_Click(object sender, EventArgs e)
+        {
+        }
+        private void lbl_Floor1_Click(object sender, EventArgs e)
+        {
+        }
+        private void btn_Floor0_panel_Click(object sender, EventArgs e)
+        {
+            btn0Clicked = true;
+            btn1Clicked = false;
+            btnUpClicked = false;
+            btnDownClicked = false;
+            //set the new floor level and display
+            floorLevel = 0;
+            lbl_Floor0.Text = floorLevel.ToString();
+            lbl_Control.Text = floorLevel.ToString();
+            lbl_Floor1.Text = floorLevel.ToString();
+            states = 3; //set the action state for the elevator
+            Insert_In_Log(); //insert into the database
+        }
+        private void btn_Floor1_panel_Click(object sender, EventArgs e)
+        {
+            btn0Clicked = false;
+            btn1Clicked = true;
+            btnUpClicked = false;
+            btnDownClicked = false;
+            //set the new floor level and display
+            floorLevel = 1;
+            lbl_Floor0.Text = floorLevel.ToString();
+            lbl_Control.Text = floorLevel.ToString();
+            lbl_Floor1.Text = floorLevel.ToString();
+            states = 4; //set the action state for the elevator
+            Insert_In_Log(); //insert into the database
+        }
+        void Insert_In_Log() //insert into the database
+        {
+            localDate = DateTime.Now; // get the current system date and time
+            //defining a new row and placing it into a new data row
+            DataRow newRow = ds.Tables[0].NewRow();
+            //get the last id (primary key) from the dataset
+            object lastID;// key number(primary key) updates each time so it's unique
+            if (ds.Tables[0].Rows.Count == 0)
+            {
+                 lastID = 0;
+            }
+            else
+            {
+                 lastID = ds.Tables[0].Rows[ds.Tables[0].Rows.Count - 1]["Part"];
+            }
+            a++; 
+            newRow["Part"] = Convert.ToInt32(lastID) + 1;
+            //different elevator actions set by the states
+            switch(states)
+            {
+                case 1:
+                    newRow["ActionMove"] = " Elevator called at ground floor at: ";
+                    break;
+                case 2:
+                    newRow["ActionMove"] = " Elevator called at first floor at: ";
+                    break;
+                case 3:
+                    newRow["ActionMove"] = " Elevator going to ground floor at: ";
+                    break;
+                case 4:
+                    newRow["ActionMove"] = " Elevator going to first floor at: ";
+                    break;
+            } 
+            newRow["DateOfAct"] = localDate.ToString() ;
+            //add the new dataRow to dataTable
+            ds.Tables[0].Rows.Add(newRow);
+            save_data_db();
+            listbox_update();
+        }
+        public void listbox_update()
+        {
+            lst_Events.Items.Clear(); // clear listbox before update
+            foreach (DataRow row in ds.Tables[0].Rows) //update listbox
+            {
+                lst_Events.Items.Add(row["Part"] + " " + row["ActionMove"] + " (" + row["DateOfAct"] + ") ");
+            }
+        }
+        private void btn_LoadEvents_Click(object sender, EventArgs e) 
+        {   // display database in the listbox
+            reload_db_to_ds();
+            listbox_update();
+        }
+        private void btn_ClearLog_Click(object sender, EventArgs e)
+        {
+            lst_Events.Items.Clear(); // clear listbox
+        }
+        private void lst_Events_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+        private void Floor1_Paint(object sender, PaintEventArgs e)
+        {
+        }
+    }
+}
